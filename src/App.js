69fa3902286115f702d47af7de54b27588b244d4
Emlak-hossain <imranmarin1990@gmail.com>
2023-02-11 01:09:30 +0600

    added footer pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './Pages/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import Footer from './Pages/Footer/Footer';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
       </Routes>
-
+      <Footer></Footer>
 
 
     </div>
